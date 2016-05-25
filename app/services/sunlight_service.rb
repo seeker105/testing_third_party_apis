ce3f1e1c89f5d0363a069f7c81b476ea4c72df42
Christopher Soden <karavol27@gmail.com>
2016-05-25 10:22:46 -0600

    Created test - no implementation yet

---
+++ app/services/sunlight_service.rb
@@ -0,0 +1,3 @@
+class SunlightService
+
+end
---
+++ spec/services/sunlight_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe SunlightService do
+  it "doesn't break" do
+    expect(1+1).to eq(2)
+  end
+
+  it "returns a list of legislators filtered by 'gender'" do
+    service = SunlightService.new
+    legislators = service.legislators(gender: "F")
+    legislator = legislators.first
+
+    expect(legislators).to eq(20)
+    expect(legislator.first_name).to eq("Joni")
+    expect(legislator.last_name).to eq("Ernst")
+  end
+end
